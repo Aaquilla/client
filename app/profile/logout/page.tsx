d32002acd4819533ce8ba89e19a9e4a0e92e51e4
webshining <webshining@protonmail.com>
2026-03-19 17:32:21 +0200

    refresh page for reset mem

--- app/profile/logout/page.tsx
+++ app/profile/logout/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const page = () => {
-	const router = useRouter();
-
 	useEffect(() => {
 		localStorage.clear();
 		fetch("/api/logout", { credentials: "include" }).then(() => {
-			router.push("/");
+			window.location.replace("/");
 		});
-	}, [router]);
+	}, []);
 	return null;
 };
 
